2b47d72632df658ff10afbdb9d3ecaaad60152a3
erdnaxe <alexandre.iooss@sfr.fr>
2014-02-19 18:19:14 +0100

    Translating comments and creating a php script. Adding the possiblity to know the original file of a object in javascript.

--- app/include/objects-choice.include.php
+++ app/include/objects-choice.include.php
@@ -5,26 +5,27 @@ if (!isset($main_loaded)) {
 }
 
 /*
- * Selecteur d'objets
+ * Objects selector
  * --------------------------
- * Ce code fait partie du projet Graf2Ard
- * Il a été créé par erdnaxe.
- * Il est sous license GPL.
+ * This code is a part of the Graf2Ard project.
+ * He has been created by erdnaxe.
+ * This project is under GPLv2 license.
  */
 
-// Création des tableaux de stockage 
+// Creating the main arrays
 $list_file = array();
 $menu = array();
 
+// Listing all JSON files in assets/objects/
 $objects_directory_open = opendir($objects_dir) or die('Erreur');
 while ($Entry = \readdir($objects_directory_open)) {
-    if ($Entry != '.' && $Entry != '..' && strstr($Entry, '.json')) {
+    if (strstr($Entry, '.json')) {
         $list_file[] = $Entry;
     }
 }
 closedir($objects_directory_open);
 
-// Exploration du fichier
+// Decoding the JSON file then filling an array
 $obj = array();
 foreach ($list_file as $id => $file) {
     // Décodage du fichier --> $obj
@@ -34,38 +35,6 @@ foreach ($list_file as $id => $file) {
         return;
     }
 
-    // DEBUG : Affichage des infos du fichier
-    //~ Nom de l'élément : $obj["name"]
-    //~ Nom de l'élément en français : ".$obj["translation_fr"]
-    //~ Position dans le menu : ".$obj["menu"]
-    //~ Librairies nécessaires : ".$obj["needing_librairies"]
-    //~ Couleur : ".$obj["color"]
-    //~ ---- Propriétés :
-    //~ foreach($obj["properties"] as $id_properties => $content_properties){
-    //~ echo "<ul>";
-    //~ echo "<li>Nom : ".$content_properties["name"]."</li>";
-    //~ foreach($content_properties["body"] as $id_body => $content_body){
-    //~ echo "<ul>";
-    //~ echo "<li>Nom : ".$content_body["name"]."</li>";
-    //~ echo "<li>Entre dans : ".$content_body["enter_in"]."</li>";
-    //~ echo "<li>Valeures autorisées : <ul>";
-    //~ foreach($content_body["legal_value"] as $id_legal_value => $value_legal_value){
-    //~ echo "<li>".$value_legal_value."</li>";
-    //~ }
-    //~ echo "</ul></li>";
-    //~ echo "</ul></br>";
-    //~ }
-    //~ echo "</ul>";
-    //~ }
-    //~ ---- Code :
-    //~ foreach($obj["code"] as $id_code => $content_code){
-    //~ echo "<ul>";
-    //~ echo "<li>Fonction : ".$content_code["function"]."</li>";
-    //~ echo "<li>Code : ".$content_code["code"]."</li>";
-    //~ echo "</ul></br>";
-    //~ }
-    //~ echo "</br>";
-
     if (!isset($obj["color"])) {
         $obj["color"] = "";
     }
@@ -74,11 +43,13 @@ foreach ($list_file as $id => $file) {
         $obj["translation_fr"] = $obj["name"];
     }
 
-    // Remplissage d'un tableau pour trier les objets par menus
-    $menu[$obj["menu"]][] = array("text" => $obj["translation_fr"], "color" => $obj["color"]);
+    // Filling an array with data (name, name in french and color)
+    $menu[$obj["menu"]][] = array("text" => $obj["name"], 
+        "text-fr" => $obj["translation_fr"], 
+        "color" => $obj["color"]);
 }
 
-//Dessinage des menus
+// Now it's time to draw dropdowns
 foreach ($menu as $name => $object) {
     ?>
     <div class="btn-group">
@@ -88,9 +59,14 @@ foreach ($menu as $name => $object) {
         <ul class="dropdown-menu">
             <?php
             foreach ($object as $array_data) {
-                echo '<li><a href="#" id="objects-list" name="' . $array_data["text"] . '" color="' . $array_data["color"] . '">';
-                echo '<span class="glyphicon glyphicon-stop ' . $array_data["color"] . '"> </span> ' . $array_data["text"];
-                echo '</a></li>';
+                echo '<li>';
+                echo '<a href="#" id="objects-list" name="' . $array_data["text"] 
+                        . '" title="' . $array_data["text-fr"] 
+                        . '" color="' . $array_data["color"] . '">';
+                echo '<span class="glyphicon glyphicon-stop ' . $array_data["color"] 
+                        . '"> </span> ' . $array_data["text"];
+                echo '</a>';
+                echo '</li>';
             }
             ?>    
         </ul>
---
+++ app/scripts/arduino_code.generator.php
@@ -0,0 +1,32 @@
+<?php
+
+// Nom de l'élément : $obj["name"]
+// Nom de l'élément en français : ".$obj["translation_fr"]
+// Position dans le menu : ".$obj["menu"]
+// Librairies nécessaires : ".$obj["needing_librairies"]
+// Couleur : ".$obj["color"]
+// ---- Propriétés :
+// foreach($obj["properties"] as $id_properties => $content_properties){
+// echo "<ul>";
+// echo "<li>Nom : ".$content_properties["name"]."</li>";
+// foreach($content_properties["body"] as $id_body => $content_body){
+// echo "<ul>";
+// echo "<li>Nom : ".$content_body["name"]."</li>";
+// echo "<li>Entre dans : ".$content_body["enter_in"]."</li>";
+// echo "<li>Valeures autorisées : <ul>";
+// foreach($content_body["legal_value"] as $id_legal_value => $value_legal_value){
+// echo "<li>".$value_legal_value."</li>";
+// }
+// echo "</ul></li>";
+// echo "</ul></br>";
+// }
+// echo "</ul>";
+// }
+// ---- Code :
+// foreach($obj["code"] as $id_code => $content_code){
+// echo "<ul>";
+// echo "<li>Fonction : ".$content_code["function"]."</li>";
+// echo "<li>Code : ".$content_code["code"]."</li>";
+// echo "</ul></br>";
+// }
+// echo "</br>";
